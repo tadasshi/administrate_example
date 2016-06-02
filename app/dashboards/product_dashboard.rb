535a8050ecc0251c322cbf4f873e27be65ea6e9a
tadasshi <Tadasshi*147>
2016-06-01 22:22:35 -0300

    administrate gem by example

---
+++ app/dashboards/product_dashboard.rb
@@ -0,0 +1,63 @@
+require "administrate/base_dashboard"
+
+class ProductDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    name: Field::String,
+    price: Field::Number.with_options(
+        title: "Unit Price",
+        prefix: "$",
+        multiplier: 0.01,
+        decimals: 2,
+        required: true
+    ),
+    description: Field::Text,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :id,
+    :name,
+    :price,
+    :description,
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :id,
+    :name,
+    :price,
+    :description,
+    :created_at,
+    :updated_at,
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :name,
+    :price,
+    :description,
+  ].freeze
+
+  # Overwrite this method to customize how products are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(product)
+  #   "Product ##{product.id}"
+  # end
+end
